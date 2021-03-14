ab4dc9156a1748e40da84ba52ca65f6715ec4581
Damir Say <dem_say@mail.ru>
2021-03-14 13:34:59 +0400

    add button actions

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
-//for Device position
-if ('DeviceOrientationEvent' in window) {
-  window.addEventListener('deviceorientation', deviceOrientationHandler, false);
-} else {
-  document.getElementById('logoContainer').innerText = 'Device Orientation API not supported.';
-}
+//btn-functions
+function startRecordingData() {
+  //for Device position
+  if ('DeviceOrientationEvent' in window) {
+    window.addEventListener('deviceorientation', deviceOrientationHandler, false);
+  } else {
+    document.getElementById('logoContainer').innerText = 'Device Orientation API not supported.';
+  }
 
-function deviceOrientationHandler (eventData) {
-  var tiltLR = eventData.gamma;
-  var tiltFB = eventData.beta;
-  var dir = eventData.alpha;
+  function deviceOrientationHandler (eventData) {
+    var tiltLR = eventData.gamma;
+    var tiltFB = eventData.beta;
+    var dir = eventData.alpha;
+    
+    document.getElementById("doTiltLR").innerHTML = Math.round(tiltLR);
+    document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
+    document.getElementById("doDirection").innerHTML = Math.round(dir);
+  }
+};
+
+function stopRecordingData() {
   
-  document.getElementById("doTiltLR").innerHTML = Math.round(tiltLR);
-  document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
-  document.getElementById("doDirection").innerHTML = Math.round(dir);
-}
+};
+
+
 
 
 
